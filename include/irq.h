b45fc0f1a2f75f53f7f82ff64c313ee6c757a5d0
oblique <psyberbits@gmail.com>
2012-11-13 07:05:42 +0200

    add irq_get_flags() and irq_set_flags()

--- include/irq.h
+++ include/irq.h
@@ -24,4 +24,31 @@ irq_disable(void)
 	asm volatile("cpsid if" : : : "memory");
 }
 
+#define CPSR_MASK_IRQ	(1<<7)
+#define CPSR_MASK_FIQ	(1<<6)
+
+static inline u32
+irq_get_flags()
+{
+	u32 val;
+	asm volatile("mrs %0, cpsr" : "=r" (val) : : "memory");
+	val &= CPSR_MASK_IRQ | CPSR_MASK_FIQ;
+	return val;
+}
+
+static inline void
+irq_set_flags(u32 flags)
+{
+	flags &= CPSR_MASK_IRQ | CPSR_MASK_FIQ;
+	asm volatile (
+		"mrs v1, cpsr		\n\t"
+		"bic v1, v1, %0		\n\t"
+		"orr v1, v1, %1		\n\t"
+		"msr cpsr_c, v1		\n\t"
+		:
+		: "r" (CPSR_MASK_IRQ | CPSR_MASK_FIQ), "r" (flags)
+		: "v1", "memory"
+	);
+}
+
 #endif	/* __IRQ_H */

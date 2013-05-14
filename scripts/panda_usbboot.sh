539c277fe0be8c9f7c04dff1d7f6808014aa975a
oblique <psyberbits@gmail.com>
2013-05-15 00:40:10 +0300

    Add omap4boot as a submodule

--- scripts/panda_usbboot.sh
+++ scripts/panda_usbboot.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
-if [ $# -eq 0 ]; then
-    BIN="usbbootImage"
-else
-    BIN=$1
+
+if [ $# -gt 0 ]; then
+    BIN=$(realpath "$1")
 fi
-USBBOOT_PATH="${HOME}/embedded/panda/omap4boot/out/panda"
-sudo ${USBBOOT_PATH}/usbboot ${USBBOOT_PATH}/aboot.bin $BIN
+
+[ $(basename ${PWD}) = "scripts" ] && cd ..
+
+if [ -z "$BIN" ]; then
+    BIN="${PWD}/usbbootImage"
+fi
+
+if [ ! -f "$BIN" ]; then
+    echo "run \`make' first."
+    exit 1
+fi
+
+if [ -z "$OMAP4BOOT_PATH" ]; then
+    OMAP4BOOT_PATH="${PWD}/usbboot/omap4boot/out/panda"
+    if [ ! -f "${OMAP4BOOT_PATH}/usbboot" -o ! -f "${OMAP4BOOT_PATH}/aboot.bin" ]; then
+        git submodule init || exit 1
+        git submodule update || exit 1
+        cd usbboot/omap4boot || exit 1
+        git reset --hard
+        patch -p1 -i ../omap4boot_remove_signature_check.patch
+        if [ -z "$TOOLCHAIN" ]; then
+            make TOOLCHAIN=arm-none-eabi-
+        else
+            make TOOLCHAIN="$TOOLCHAIN"
+        fi
+    fi
+fi
+
+sudo "${OMAP4BOOT_PATH}/usbboot" "${OMAP4BOOT_PATH}/aboot.bin" "${BIN}"

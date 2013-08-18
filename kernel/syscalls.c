3014676a7f3803961c64c177f9dc6ea3eaec157f
oblique <psyberbits@gmail.com>
2013-08-18 06:44:46 +0300

    Use the same calling convetion that Linux kernel use for syscalls.
    SWI/SVC handler use Linux ARM EABI calling convention:
    r0 - r6 registers are used for the syscall arguments, it can take up to 7 arguments.
    r7 register is used for the syscall number.
    "swi #0" is used from calling the syscall.

--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -1,26 +1,30 @@
 #include <kernel.h>
 
 long
-sys_test_6_args(u32 a1, u32 a2, u32 a3, u32 a4, u32 a5, u32 a6)
+sys_test_1_args(u32 a1)
 {
-	kprintf("sys_test_6_args(%d, %d, %d, %d, %d, %d)\n", a1, a2, a3, a4, a5, a6);
+	kprintf("sys_test_1_args(%d)\n", a1);
 	return 0;
 }
 
-struct __test_7_args {
-	u32 a1;
-	u32 a2;
-	u32 a3;
-	u32 a4;
-	u32 a5;
-	u32 a6;
-	u32 a7;
-};
 
 long
-sys_test_7_args(struct __test_7_args *a)
+sys_test_4_args(u32 a1, u32 a2, u32 a3, u32 a4)
 {
-	kprintf("sys_test_7_args(%d, %d, %d, %d, %d, %d, %d)\n",
-		a->a1, a->a2, a->a3, a->a4, a->a5, a->a6, a->a7);
+	kprintf("sys_test_4_args(%d, %d, %d, %d)\n", a1, a2, a3, a4);
+	return 0;
+}
+
+long
+sys_test_5_args(u32 a1, u32 a2, u32 a3, u32 a4, u32 a5)
+{
+	kprintf("sys_test_5_args(%d, %d, %d, %d, %d)\n", a1, a2, a3, a4, a5);
+	return 0;
+}
+
+long
+sys_test_7_args(u32 a1, u32 a2, u32 a3, u32 a4, u32 a5, u32 a6, u32 a7)
+{
+	kprintf("sys_test_7_args(%d, %d, %d, %d, %d, %d, %d)\n", a1, a2, a3, a4, a5, a6, a7);
 	return 0;
 }
